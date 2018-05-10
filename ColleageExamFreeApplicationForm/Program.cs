ec7492977920c91bdc4cc10ef9b55d8f93ad362f
ED <yinghua.lin@ischool.com.tw>
2018-05-10 15:34:08 +0800

    2018/05/10 穎驊整理調整 五專免試入學功能

--- ColleageExamFreeApplicationForm/Program.cs
+++ ColleageExamFreeApplicationForm/Program.cs
@@ -13,8 +13,8 @@ namespace ColleageExamFreeApplicationForm
         public static void main()
         {
             FISCA.Presentation.RibbonBarItem item1 = FISCA.Presentation.MotherForm.RibbonBarItems["學生", "資料統計"];
-            item1["報表"]["成績相關報表"]["五專免試入學相關報表"]["五專集體報名表"].Enable = false;
-            item1["報表"]["成績相關報表"]["五專免試入學相關報表"]["五專集體報名表"].Click += delegate
+            item1["報表"]["成績相關報表"]["107年五專(優先)免試入學相關報表"]["北中南五專(優先)免試入學集體報名表"].Enable = false;
+            item1["報表"]["成績相關報表"]["107年五專(優先)免試入學相關報表"]["北中南五專(優先)免試入學集體報名表"].Click += delegate
             {
                 Report report = new Report();
                 report.ShowDialog();
@@ -24,17 +24,17 @@ namespace ColleageExamFreeApplicationForm
             {
                 if (K12.Presentation.NLDPanels.Student.SelectedSource.Count > 0 && Permissions.五專集體報名表權限)
                 {
-                    item1["報表"]["成績相關報表"]["五專免試入學相關報表"]["五專集體報名表"].Enable = true;
+                    item1["報表"]["成績相關報表"]["107年五專(優先)免試入學相關報表"]["北中南五專(優先)免試入學集體報名表"].Enable = true;
                 }
                 else
                 {
-                    item1["報表"]["成績相關報表"]["五專免試入學相關報表"]["五專集體報名表"].Enable = false;
+                    item1["報表"]["成績相關報表"]["107年五專(優先)免試入學相關報表"]["北中南五專(優先)免試入學集體報名表"].Enable = false;
                 }
             };
 
             //權限設定
             Catalog permission = RoleAclSource.Instance["學生"]["功能按鈕"];
-            permission.Add(new RibbonFeature(Permissions.五專集體報名表, "五專集體報名表"));
+            permission.Add(new RibbonFeature(Permissions.五專集體報名表, "北中南五專(優先)免試入學集體報名表"));
         }
     }
 }
